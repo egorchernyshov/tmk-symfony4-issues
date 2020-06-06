42f1c00ecfd52c8755f678a874d1520305d21bc6
Georgy Chernyshov <georgy.chernyshov@gmail.com>
2020-06-06 11:47:39 +0300

    Adding Category entity

---
+++ src/Entity/Category.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Constraints\Type(type="string")
+     * @Constraints\NotBlank()
+     * @Constraints\Length(
+     *     min="3",
+     *     max="12",
+     *     minMessage="Title must be at least {{ limit }} characters long",
+     *     maxMessage="Title cannot be longer than {{ limit }} characters",
+     *     allowEmptyString="false"
+     * )
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     *
+     * @Constraints\Type(type="integer")
+     * @Constraints\PositiveOrZero()
+     */
+    private $eId;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Product::class, mappedBy="categories")
+     */
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getEId(): ?int
+    {
+        return $this->eId;
+    }
+
+    public function setEId(?int $eId): self
+    {
+        $this->eId = $eId;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (! $this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->addCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->contains($product)) {
+            $this->products->removeElement($product);
+            $product->removeCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->title;
+    }
+}
